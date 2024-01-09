6c677c30eda5541519fd285d9bd9f43fd33fe359
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2024-01-09 20:52:57 +0530

    Create Search-Pattern-KMP-Algorithm-Solution-.cpp

---
+++ Search-Pattern-KMP-Algorithm-Solution-.cpp
@@ -0,0 +1,95 @@
+******************************************************************  Question  **************************************************************************
+Given two strings, one is a text string, txt and other is a pattern string, pat. The task is to print the indexes of all the occurences of pattern string in the text string. Use one-based indexing while returing the indices. 
+Note: Return an empty list incase of no occurences of pattern. Driver will print -1 in this case.
+
+Example 1:
+
+Input:
+txt = "geeksforgeeks"
+pat = "geek"
+Output: 
+1 9
+Explanation: 
+The string "geek" occurs twice in txt, one starts are index 1 and the other at index 9. 
+Example 2:
+
+Input: 
+txt = "abesdu"
+pat = "edu"
+Output: 
+-1
+Explanation: 
+There's not substring "edu" present in txt.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function search() which takes the string txt and the string pat as inputs and returns an array denoting the start indices (1-based) of substring pat in the string txt. 
+
+Expected Time Complexity: O(|txt|).
+Expected Auxiliary Space: O(|txt|).
+
+Constraints:
+1 ≤ |txt| ≤ 105
+1 ≤ |pat| < |S|
+Both the strings consists of lowercase English alphabets.
+
+
+
+
+
+******************************************************************  Solution  **************************************************************************
+
+
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        vector <int> search(string pat, string txt)
+        {
+            //code here
+            vector<int>ans;
+            
+            int i =0;
+            int idx = txt.find(pat);
+            
+            while(idx != -1){
+                
+                ans.push_back(idx+1);
+                
+                idx = txt.find(pat,idx+1);
+            }
+            
+            return ans.empty()?vector<int>{-1} : ans;
+            
+        }
+     
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string S, pat;
+        cin >> S >> pat;
+        Solution ob;
+        vector <int> res = ob.search(pat, S);
+        if (res.size()==0)
+            cout<<-1<<endl;
+        else {
+            for (int i : res) cout << i << " ";
+            cout << endl;
+        }
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
